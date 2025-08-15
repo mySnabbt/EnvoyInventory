64bc5dd7cd7c2ad5248047342798ec2447bf38be
vaibhav250904 <vaibhav.gupta@students.mq.edu.au>
2025-08-15 15:28:51 +1000

    changes in env

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config';
 
-console.log('Using SUPABASE_URL:', process.env.SUPABASE_URL);
-console.log('Using SUPABASE_SERVICE_ROLE_KEY:', process.env.SUPABASE_SERVICE_ROLE_KEY?.slice(0,12) + '...');
+console.log('Using SUPABASE_URL:', process.env.REACT_APP_SUPABASE_URL);
+console.log('Using SUPABASE_SERVICE_ROLE_KEY:', process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY?.slice(0,12) + '...');
 
 
 console.log(
   '[supabase] using key:',
-  process.env.SUPABASE_SERVICE_ROLE_KEY ? 'SERVICE_ROLE_KEY' : '<<no key>>'
+  process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY ? 'SERVICE_ROLE_KEY' : '<<no key>>'
 );
 
-console.log('[supabase] using URL:', process.env.SUPABASE_URL);
+console.log('[supabase] using URL:', process.env.REACT_APP_SUPABASE_URL);
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,  // ✅ must be exactly correct
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY,  // ✅ must be exactly correct
   {
     auth: {
       persistSession: false
